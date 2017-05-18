99ea172c30d76b6cfa19e0e7ef61d8bd6f939853
Guido Vilariño <gvilarino@gmail.com>
2017-05-18 18:33:33 -0300

    Remove close alert when user is signed out (#143)
    * Remove close browser tab alert when signed out
    
    * Remove whitespace line

--- www/assets/app.js
+++ www/assets/app.js
@@ -23,7 +23,7 @@
         $scope.newInstanceBtnText = '+ Add new instance';
         $scope.deleteInstanceBtnText = 'Delete';
         $scope.isInstanceBeingDeleted = false;
-        
+
         var selectedKeyboardShortcuts = KeyboardShortcutService.getCurrentShortcuts();
 
         angular.element($window).bind('resize', function() {
@@ -55,6 +55,8 @@
 	KeyboardShortcutService.setResizeFunc($scope.resize);
 
         $scope.closeSession = function() {
+            // Remove alert before closing browser tab
+            window.onbeforeunload = null;
             $scope.socket.emit('session close');
         }
 
