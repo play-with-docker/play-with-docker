fa3b56ecfe1df59d8cf535b6429f8bd22ac167e2
Marcos Lilljedahl <marcosnils@gmail.com>
2017-07-31 12:00:49 -0300

    Check docker daemon is alive in ping endpoint

--- handlers/ping.go
+++ handlers/ping.go
@@ -1,15 +1,33 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/play-with-docker/play-with-docker/config"
 	"github.com/shirou/gopsutil/load"
 )
 
 func Ping(rw http.ResponseWriter, req *http.Request) {
 	// Get system load average of the last 5 minutes and compare it against a threashold.
+
+	c, err := client.NewEnvClient()
+
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Microsecond)
+	defer cancel()
+
+	if _, err := c.Info(ctx); err != nil && err == context.DeadlineExceeded {
+		log.Printf("Docker info took to long to respond\n")
+		rw.WriteHeader(http.StatusGatewayTimeout)
+	}
 
 	a, err := load.Avg()
 	if err != nil {
