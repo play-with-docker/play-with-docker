2cbde6d067c344f92ef6536fc730827e104a37fc
Marcos Nils <marcosnils@gmail.com>
2017-11-02 14:20:14 -0300

    Remove session lock as it's worthless now that PWD is deployed in (#204)
    multiple servers

--- pwd/types/session.go
+++ pwd/types/session.go
@@ -1,28 +1,18 @@
 package types
 
 import (
-	"sync"
 	"time"
 )
 
 type Session struct {
-	Id           string     `json:"id" bson:"id"`
-	CreatedAt    time.Time  `json:"created_at" bson:"created_at"`
-	ExpiresAt    time.Time  `json:"expires_at" bson:"expires_at"`
-	PwdIpAddress string     `json:"pwd_ip_address" bson:"pwd_ip_address"`
-	Ready        bool       `json:"ready" bson:"ready"`
-	Stack        string     `json:"stack" bson:"stack"`
-	StackName    string     `json:"stack_name" bson:"stack_name"`
-	ImageName    string     `json:"image_name" bson:"image_name"`
-	Host         string     `json:"host" bson:"host"`
-	UserId       string     `json:"user_id" bson:"user_id"`
-	rw           sync.Mutex `json:"-"`
-}
-
-func (s *Session) Lock() {
-	s.rw.Lock()
-}
-
-func (s *Session) Unlock() {
-	s.rw.Unlock()
+	Id           string    `json:"id" bson:"id"`
+	CreatedAt    time.Time `json:"created_at" bson:"created_at"`
+	ExpiresAt    time.Time `json:"expires_at" bson:"expires_at"`
+	PwdIpAddress string    `json:"pwd_ip_address" bson:"pwd_ip_address"`
+	Ready        bool      `json:"ready" bson:"ready"`
+	Stack        string    `json:"stack" bson:"stack"`
+	StackName    string    `json:"stack_name" bson:"stack_name"`
+	ImageName    string    `json:"image_name" bson:"image_name"`
+	Host         string    `json:"host" bson:"host"`
+	UserId       string    `json:"user_id" bson:"user_id"`
 }
