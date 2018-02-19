54b961a950698b9c5976df20490db6f994eb2ad1
Marcos Lilljedahl <marcosnils@gmail.com>
2018-02-19 00:21:19 -0300

    Update Gorilla handlers and add CORS option

--- handlers/bootstrap.go
+++ handlers/bootstrap.go
@@ -62,7 +62,7 @@ func Register(extend HandlerExtender) {
 			return true
 		}
 		return false
-	}))
+	}), gh.AllowedHeaders([]string{}))
 
 	// Specific routes
 	r.HandleFunc("/ping", Ping).Methods("GET")
