cf135586159dac6228b4cfb12cd6fb98270d89ed
Marcos Lilljedahl <marcosnils@gmail.com>
2018-04-25 13:23:48 -0300

    Add PWK

--- handlers/bootstrap.go
+++ handlers/bootstrap.go
@@ -60,6 +60,7 @@ func Register(extend HandlerExtender) {
 	corsHandler := gh.CORS(gh.AllowCredentials(), gh.AllowedHeaders([]string{"x-requested-with", "content-type"}), gh.AllowedMethods([]string{"GET", "POST", "HEAD", "DELETE"}), gh.AllowedOriginValidator(func(origin string) bool {
 		if strings.Contains(origin, "localhost") ||
 			strings.HasSuffix(origin, "play-with-docker.com") ||
+			strings.HasSuffix(origin, "play-with-kuberentes.com") ||
 			strings.HasSuffix(origin, "play-with-moby.com") {
 			return true
 		}
