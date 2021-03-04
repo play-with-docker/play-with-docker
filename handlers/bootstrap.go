c0aaf0a3a4a1cf172b06f4873f28bb5b19ca1152
Marcos Lilljedahl <marcosnils@gmail.com>
2021-03-04 00:16:11 -0300

    Fix Users handler. This never worked :D
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- handlers/bootstrap.go
+++ handlers/bootstrap.go
@@ -117,7 +117,7 @@ func Register(extend HandlerExtender) {
 	r.HandleFunc("/", Landing).Methods("GET")
 
 	corsRouter.HandleFunc("/users/me", LoggedInUser).Methods("GET")
-	r.HandleFunc("/users/{userId:^(?me)}", GetUser).Methods("GET")
+	r.HandleFunc("/users/{userId:.{3,}}", GetUser).Methods("GET")
 	r.HandleFunc("/oauth/providers", ListProviders).Methods("GET")
 	r.HandleFunc("/oauth/providers/{provider}/login", Login).Methods("GET")
 	r.HandleFunc("/oauth/providers/{provider}/callback", LoginCallback).Methods("GET")
