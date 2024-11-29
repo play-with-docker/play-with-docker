1f8c1ee7f0fc583cce61a1f44a925e50687b9eb9
joaquinito2070 <118765355+joaquinito2070@users.noreply.github.com>
2024-11-29 16:01:02 +0100

    Update playground.go
    Adds JWT to prevent futures closes.

--- pwd/playground.go
+++ pwd/playground.go
@@ -1,15 +1,54 @@
 package pwd
 
 import (
+	"bytes"
+	"compress/zlib"
+	"crypto/rand"
+	"encoding/base64"
 	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/play-with-docker/play-with-docker/event"
 	"github.com/play-with-docker/play-with-docker/pwd/types"
 	"github.com/satori/go.uuid"
 )
 
+func encodeJWT(token string) string {
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	w.Write([]byte(token))
+	w.Close()
+	
+	// Encode multiple times (50-100) to make it harder to decode
+	encoded := base64.StdEncoding.EncodeToString(b.Bytes())
+	iterations := 50 + rand.Intn(51) // Random number between 50-100
+	
+	for i := 0; i < iterations; i++ {
+		encoded = base64.StdEncoding.EncodeToString([]byte(encoded))
+	}
+	
+	return encoded
+}
+
 func (p *pwd) PlaygroundNew(playground types.Playground) (*types.Playground, error) {
 	playground.Id = uuid.NewV5(uuid.NamespaceOID, playground.Domain).String()
+	
+	// Generate JWT token
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = playground.Domain
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		log.Printf("Error generating JWT for playground %s. Got: %v\n", playground.Id, err)
+		return nil, err
+	}
+	
+	// Encode and store the JWT
+	playground.JWT = encodeJWT(tokenString)
+	
 	if err := p.storage.PlaygroundPut(&playground); err != nil {
 		log.Printf("Error saving playground %s. Got: %v\n", playground.Id, err)
 		return nil, err
@@ -24,15 +63,31 @@ func (p *pwd) PlaygroundGet(id string) *types.Playground {
 		log.Printf("Error retrieving playground %s. Got: %v\n", id, err)
 		return nil
 	} else {
+		// Don't expose JWT in response
+		playground.JWT = ""
 		return playground
 	}
 }
 
 func (p *pwd) PlaygroundFindByDomain(domain string) *types.Playground {
 	id := uuid.NewV5(uuid.NamespaceOID, domain).String()
-	return p.PlaygroundGet(id)
+	playground := p.PlaygroundGet(id)
+	if playground != nil {
+		// Don't expose JWT in response
+		playground.JWT = ""
+	}
+	return playground
 }
 
 func (p *pwd) PlaygroundList() ([]*types.Playground, error) {
-	return p.storage.PlaygroundGetAll()
+	playgrounds, err := p.storage.PlaygroundGetAll()
+	if err != nil {
+		return nil, err
+	}
+	
+	// Don't expose JWTs in response
+	for _, playground := range playgrounds {
+		playground.JWT = ""
+	}
+	return playgrounds, nil
 }
